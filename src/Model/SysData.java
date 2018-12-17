1936d030be7e6a9f7bdfcab7829e1f7880d93a21
ADI-YOGA\Adi <adibrucker2@gmail.com>
2018-12-17 15:33:21 +0200

    fix

--- src/Model/SysData.java
+++ src/Model/SysData.java
@@ -114,7 +114,7 @@ public class SysData implements Serializable{
 		FileWriter writer = null;
 
 		try {
-			writer = new FileWriter ("res/questions.json");
+			writer = new FileWriter ("src/res/questions.json");
 			writer.write(strJson);
 
 		} catch (IOException e) {
