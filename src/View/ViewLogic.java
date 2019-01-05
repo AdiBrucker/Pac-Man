ef24ed51c9011cae5665b0b5c53a38fd8c62d585
GUYLEVY\Guy <guyly10@gmail.com>
2019-01-05 12:48:21 +0200

    Fixing the stuck movement bug when swithching between player
    Fixing the stuck movement bug when swithching between player

--- src/View/ViewLogic.java
+++ src/View/ViewLogic.java
@@ -15,25 +15,26 @@ import java.awt.*;
  */
 public class ViewLogic {
 
-	private static Label lScore;
+    private static Label lScore;
     private static Label lifeScore;
     public static Label timer;
     private static Label nickname;
     private static ViewLogic instance;
-    private static  Game game;
-    private    int TimerCounting = 0;// counting seconds
-    private  int saveCountingToContinue = 0;// counting seconds
+    private static Game game;
+    private int TimerCounting = 0;// counting seconds
+    private int saveCountingToContinue = 0;// counting seconds
 
-    private static String timeResults="";/// 
-     private  TimerTask task;
-    public static boolean restart= false;
+    private static String timeResults = "";///
+    private TimerTask task;
+    public static boolean restart = false;
 
-    
+
     public static void SetInstance() {
-    	instance=null;
+        instance = null;
     }
-    public   String GetTimeResults() {
-    	return timeResults;
+
+    public String GetTimeResults() {
+        return timeResults;
     }
 
     public static Label getNickname() {
@@ -43,17 +44,17 @@ public class ViewLogic {
     public static ViewLogic getInstance() {
         if (instance == null) {
             instance = new ViewLogic();
-         //   game = Game.getInstance();
+            //   game = Game.getInstance();
             nickname = new Label(Game.pacmans.get(Game.getPlayerIndex()).getPacmanName());
 
-            nickname.setPreferredSize(new Dimension(300,40));
+            nickname.setPreferredSize(new Dimension(300, 40));
             nickname.setForeground(Color.WHITE);
             nickname.setFont(new java.awt.Font("Monospaced", java.awt.Font.BOLD
                     | java.awt.Font.ITALIC, 36));
             nickname.setBackground(Color.BLACK);
 
             lScore = new Label("SCORE: " + String.valueOf(Game.pacmans.get(Game.getPlayerIndex()).getScore()));
-            lScore.setPreferredSize(new Dimension(300,40));
+            lScore.setPreferredSize(new Dimension(300, 40));
             lScore.setForeground(Color.WHITE);
             lScore.setFont(new java.awt.Font("Monospaced", java.awt.Font.BOLD
                     | java.awt.Font.ITALIC, 36));
@@ -61,14 +62,14 @@ public class ViewLogic {
 
             lifeScore = new Label("LIFE SCORE: " + String.valueOf(Game.pacmans.get(Game.getPlayerIndex()).getLifeScore()));
             lifeScore.setForeground(Color.WHITE);
-            lifeScore.setMinimumSize(new Dimension(300,40));
+            lifeScore.setMinimumSize(new Dimension(300, 40));
             lifeScore.setFont(new java.awt.Font("Monospaced", java.awt.Font.BOLD
                     | java.awt.Font.ITALIC, 36));
             lifeScore.setBackground(Color.BLACK);
 
             timer = new Label("timer : 00:00");
             timer.setForeground(Color.WHITE);
-            timer.setMinimumSize(new Dimension(200,40));
+            timer.setMinimumSize(new Dimension(200, 40));
             timer.setFont(new java.awt.Font("Monospaced", java.awt.Font.BOLD
                     | java.awt.Font.ITALIC, 36));
             timer.setBackground(Color.BLACK);
@@ -80,108 +81,108 @@ public class ViewLogic {
     public static void setScoreForPacman() {
         lScore.setText("SCORE: " + Game.pacmans.get(Game.getPlayerIndex()).getScore());
     }
+
     public void setTimerCounting() {
-    	 TimerCounting = 0;
-    	 }
-    public Label getlScoreForPacman(){
+        TimerCounting = 0;
+    }
+
+    public Label getlScoreForPacman() {
         return lScore;
     }
 
     public static void setLifeScoreForPacman() {
         lifeScore.setText("LIFE SCORE: " + Game.pacmans.get(Game.getPlayerIndex()).getLifeScore());
     }
-    public Label getLifeScoreForPacman(){
-    	
+
+    public Label getLifeScoreForPacman() {
+
         return lifeScore;
     }
-    
+
     /**
      * cancel timer when there are a pause at the game
-     * the func save the time and cancel the timer 
+     * the func save the time and cancel the timer
      */
-    public  void CancelTimer() {
-    	saveCountingToContinue=TimerCounting;
-    	restart=true;
+    public void CancelTimer() {
+        saveCountingToContinue = TimerCounting;
+        restart = true;
     }
+
     /**
      * init the timer
+     *
      * @return
      */
     public void getTimer() {
-    	
-    	Timer timer1=new Timer();
-          task =new TimerTask() {
-    	    	public void run () {
 
-                    if(restart) {
-    	        		TimerCounting=saveCountingToContinue;
-	            		 task.cancel();
-	            		 timer1.cancel(); //In order to gracefully terminate the timer thread
-	            	     restart=false;
-	            	}
-     	        	TimerCounting++;
-    	            int minutes1 = (int) Math.floor(TimerCounting / 60F);
-    	            int second1 = (int) Math.floor(TimerCounting - minutes1 * 60);
-    	            
-    	            if(second1%10 == 0) {//// if 40 second passed its replace with the other users
-                        setPacmanTurn();
-                        Timer timer = new Timer();
+        Timer timer1 = new Timer();
+        task = new TimerTask() {
+            public void run() {
 
-                        timer.schedule(new TimerTask() {
-                            public void run() {
-                               if (PopUpLogic.getNumOfPlayers() >1)
+                if (restart) {
+                    TimerCounting = saveCountingToContinue;
+                    task.cancel();
+                    timer1.cancel(); //In order to gracefully terminate the timer thread
+                    restart = false;
+                }
+                TimerCounting++;
+                int minutes1 = (int) Math.floor(TimerCounting / 60F);
+                int second1 = (int) Math.floor(TimerCounting - minutes1 * 60);
+
+                if (second1 % 10 == 0) {//// if 40 second passed its replace with the other users
+                    setPacmanTurn();
+                    Timer timer = new Timer();
+
+                    timer.schedule(new TimerTask() {
+                        public void run() {
+                            if (PopUpLogic.getNumOfPlayers() > 1)
                                 PopUpLogic.getInstance().showPlayerTurn();
-                            }
-                        }, 1);
+                        }
+                    }, 1);
+
+                }
+
+
+                // checking if the second is less then 10 its will add a 0 at the begging of the second view
+                if (second1 < 10) {
+                    if (minutes1 < 10) { // checking if the minutes is less then 10 its will add a 0 at the begging of the second view
+                        timeResults = "0" + minutes1 + ":0" + second1;
+                        timer.setText("Timer: " + timeResults);
+                    } else {
+                        timeResults = "" + minutes1 + ":0" + second1;
+                        timer.setText("Timer: " + timeResults);
 
                     }
+                } else {
+                    if (minutes1 < 10) {
+                        timeResults = "0" + minutes1 + ":" + second1;
+                        timer.setText("Timer: " + timeResults);
+                    } else {
+                        timeResults = "" + minutes1 + ":" + second1;
+                        timer.setText("Timer: " + timeResults);
+                    }
+                }
 
 
+            }
+        };
+        timer1.scheduleAtFixedRate(task, 1000, 1000);/// the time will work the same as a regular timer
+        resetPacmanMovements();
+    }
 
-    	            // checking if the second is less then 10 its will add a 0 at the begging of the second view
-    	            if(second1<10) {
-    	            	if(minutes1<10) { // checking if the minutes is less then 10 its will add a 0 at the begging of the second view
-    	            		timeResults="0"+minutes1+":0"+second1;
-        	            	timer.setText("Timer: " + timeResults) ;
-    	            	}
-    	            	else {
-    	            		timeResults=""+minutes1+":0"+second1;
-        	            	timer.setText("Timer: " + timeResults) ;
 
-    	            	}
-    	            }
-    	            else {
-     	            	if(minutes1<10) {
-        	            	timeResults="0"+minutes1+":"+second1;
-         	            	timer.setText("Timer: " + timeResults) ;
-    	            	}
-    	            	else {
-        	            	timeResults=""+minutes1+":"+second1;
-         	            	timer.setText("Timer: " + timeResults) ;
-    	            	}
-    	            } 
-    	            
-     
-    	         	
-    	    	}
-    	   };
-    	timer1.scheduleAtFixedRate(task,1000,1000);/// the time will work the same as a regular timer
-      }
-    
+    public static void setPacmanTurn() {
 
-    public static void setPacmanTurn(){
-    	
-    	ArrayList<Game>GameList= Game.getInstanceList();
- 
-        if(Game.getPlayerIndex() == 0 && PopUpLogic.getNumOfPlayers() >1) {
- 
+        ArrayList<Game> GameList = Game.getInstanceList();
+
+        if (Game.getPlayerIndex() == 0 && PopUpLogic.getNumOfPlayers() > 1) {
+
             Game.setPlayerIndex(1);
             setNickname();
             setLifeScoreForPacman();
             setScoreForPacman();
-        }
-        else {
- 
+        } else {
+
             Game.setPlayerIndex(0);
             setNickname();
             setLifeScoreForPacman();
@@ -189,7 +190,18 @@ public class ViewLogic {
         }
     }
 
+    private static void resetPacmanMovements() {
+        Game.pacmans.get(0).right = false;
+        Game.pacmans.get(0).left = false;
+        Game.pacmans.get(0).up = false;
+        Game.pacmans.get(0).down = false;
+        Game.pacmans.get(1).right = false;
+        Game.pacmans.get(1).left = false;
+        Game.pacmans.get(1).up = false;
+        Game.pacmans.get(1).down = false;
+    }
+
     public static void setNickname() {
         ViewLogic.nickname.setText(Game.pacmans.get(Game.getPlayerIndex()).getPacmanName());
     }
- }
+}
